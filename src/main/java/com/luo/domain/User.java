65cbcdbcfc9a4eb05790ee3251e36cad0fbb41cf
youki <xiaohuihui1335@163.com>
2019-01-08 19:13:32 +0800

    ssm

---
+++ src/main/java/com/luo/domain/User.java
@@ -0,0 +1,25 @@
+package com.luo.domain;
+
+public class User {
+	private Integer userId;  
+    public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	private String userName;  
+    private String userPassword;  
+}
---
+++ src/main/java/com/luo/service/UserServiceimpl.java
@@ -0,0 +1,19 @@
+package com.luo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luo.dao.UserDao;
+import com.luo.domain.User;
+
+@Service  
+public class UserServiceimpl implements UserService {
+
+    @Autowired  
+    private UserDao userDao;  
+
+    public User selectUserById(Integer userId) {  
+        return userDao.selectUserById(userId);  
+
+    }  
+}
